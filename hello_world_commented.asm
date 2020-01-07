d8ba3cb7901f8be3ecfd7f104ce912a7f3b127d0
Peter <frosticless@hotmail.com>
2020-01-07 20:47:35 +0000

    Improve comments

--- hello_world_commented.asm
+++ hello_world_commented.asm
@@ -10,11 +10,16 @@ bits 16
 ; All CPUs run in 16bit mode while in the BIOS
 
 org 0x7c00 
-; Sets the origin (start address)
+; Sets the origin, which is where our program will be loaded into memory
+;
+;   The BIOS will load the program into 0x7c00 because it has
+;   0x55aa at the end of it, which means it's a bootable sector of code.
+;
 ;   0x7c00 comes from a 1981 IBM PC that used this location
-;   because MS-DOS specified a minimum of 32Kb of memory (0x8000), and
-;   the boot sector needs 512 bytes, the stack for the boot sector
-;   is also 512 bytes. 0x8000 - (512 + 512) = 0x7c00
+;   because MS-DOS required a minimum 32Kb (0x8000) of memory, and
+;   the boot sector needs 512 bytes, then the stack for the boot 
+;   sector is also 512 bytes. 0x8000 - (512 + 512) = 0x7c00
+
 
 msg: db "Hello world"   
 ; This declares a label called "msg" that contains "Hello world"
@@ -29,14 +34,14 @@ msglen equ $-msg
 
 mov si, msg           
 ; Moves the address of our message into the SI register
-;   SI is the source index register
-;   used by other instructions as a pointer to the start of a string
+;   SI is the source index register, it's often used whenever 
+;   you need to point an instruction to a chunk of data like a string
 
 mov ah, 0x0e          
 ; Moves the value 0x0e into AH
 ;   0x0e is the ID for the teletype function in the BIOS video services
 ;   AH is the High byte of register A (the first general purpose register)
-;   The BIOS reads this byte we do `int 0x10`
+;   The BIOS reads this byte when we do `int 0x10`
 
 mov bl, msglen        
 ; Moves the message length (declared above) into BL
@@ -54,20 +59,22 @@ print_char:
 
     int 0x10            
     ; Generate an interrupt that hands control over to the BIOS
-    ; 0x10 is the ID for BIOS video services
-    ; the BIOS video services then checks AH to see what it should do
+    ;   0x10 is the ID for BIOS video services
+    ;   the BIOS video services then checks AH to see what it should do
+    ;   we set this to 0x0e earlier which means the BIOS will 
+    ;   print the character in AL to the screen
 
     dec bl
     ; Decrements the value in BL
     ; this is our counter for how many characters we have left in the message
 
     cmp bl, 0           
-    ; compare the value in BL with 0
+    ; Compare the value in BL with 0
     ; This will set some flags to indicate if the values are equal or not
 
     jne print_char    
     ; Read as: jump-not-equal  
-    ; checks the flags set by the cmp instruction and jumps back to 
+    ; checks the flags set by the cmp instruction, and jumps back to 
     ; the print_char label if the value in BL is not equal to 0
 
     hlt        
@@ -77,11 +84,11 @@ print_char:
 
 
 times 510-($-$$) db 0
-; times is {make x number of copies of thing} {thing}
+; times means generate {some number of copies of thing} {thing}
 ; 510 because a boot sector is 512 bytes, and there's 2 bytes after this
-;   $ is the memory location of the current line
-;   $$ is the memory location for the start of the current section
-;   So $ - $$ is the length in bytes of the current section
+;   $ gives the memory location of the current line
+;   $$ gives the memory location for the start of the program
+;   So $ - $$ is the length in bytes of the program
 ;   db is declare an initialized byte, db 0 is declare a byte and set it to 0
 
 dw 0xaa55
